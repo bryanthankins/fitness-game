f1a0dfee05ba3c30697e0f6427289c6adafc5f99
Bryant Hankins <bryanthankins@gmail.com>
2016-11-06 19:20:25 -0600

    moving to modules

--- bin/js/game.js
+++ bin/js/game.js
@@ -1,30 +1,120 @@
-var SimpleGame = (function () {
-    function SimpleGame() {
-        // create our phaser game
-        // 800 - width
-        // 600 - height
-        // Phaser.AUTO - determine the renderer automatically (canvas, webgl)
-        // { preload:this.preload, create:this.create} - functions to call for our states
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
-    }
-    SimpleGame.prototype.preload = function () {
-        // add our logo image to the assets class under the
-        // key 'logo'. We're also setting the background colour
-        // so it's the same as the background colour in the image
-        this.game.load.image('logo', "assets/ds_logo.png");
-        this.game.stage.backgroundColor = 0xB20059;
-    };
-    SimpleGame.prototype.create = function () {
-        // add the 'logo' sprite to the game, position it in the
-        // center of the screen, and set the anchor to the center of
-        // the image so it's centered properly. There's a lot of
-        // centering in that last sentence
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-    };
-    return SimpleGame;
-}());
-// when the page has finished loading, create our game
-window.onload = function () {
-    var game = new SimpleGame();
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+window.onload = function () {
+    var game = new FitnessGame.Game();
+};
+var FitnessGame;
+(function (FitnessGame) {
+    var Boot = (function (_super) {
+        __extends(Boot, _super);
+        function Boot() {
+            _super.apply(this, arguments);
+        }
+        Boot.prototype.preload = function () {
+            this.load.image('preloadBar', 'assets/loader.png');
+        };
+        Boot.prototype.create = function () {
+            //  Unless you specifically need to support multitouch I would recommend setting this to 1
+            this.input.maxPointers = 1;
+            //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
+            this.stage.disableVisibilityChange = true;
+            this.game.state.start('Preloader', true, false);
+        };
+        return Boot;
+    }(Phaser.State));
+    FitnessGame.Boot = Boot;
+})(FitnessGame || (FitnessGame = {}));
+var FitnessGame;
+(function (FitnessGame) {
+    var Game = (function (_super) {
+        __extends(Game, _super);
+        function Game() {
+            _super.call(this, 800, 600, Phaser.AUTO, 'content', null);
+            this.state.add('Boot', FitnessGame.Boot, false);
+            this.state.add('Preloader', FitnessGame.Preloader, false);
+            this.state.add('MainMenu', FitnessGame.MainMenu, false);
+            this.state.add('Level1', FitnessGame.Level1, false);
+            this.state.start('Boot');
+        }
+        return Game;
+    }(Phaser.Game));
+    FitnessGame.Game = Game;
+})(FitnessGame || (FitnessGame = {}));
+var FitnessGame;
+(function (FitnessGame) {
+    var Level1 = (function (_super) {
+        __extends(Level1, _super);
+        function Level1() {
+            _super.apply(this, arguments);
+        }
+        //player: Castlevania.Player;
+        Level1.prototype.create = function () {
+            this.background = this.add.sprite(0, 0, 'level1');
+            this.music = this.add.audio('music', 1, false);
+            this.music.play();
+            //this.player = new Player(this.game, 130, 284);
+        };
+        return Level1;
+    }(Phaser.State));
+    FitnessGame.Level1 = Level1;
+})(FitnessGame || (FitnessGame = {}));
+var FitnessGame;
+(function (FitnessGame) {
+    var MainMenu = (function (_super) {
+        __extends(MainMenu, _super);
+        function MainMenu() {
+            _super.apply(this, arguments);
+        }
+        MainMenu.prototype.create = function () {
+            this.background = this.add.sprite(0, 0, 'titlepage');
+            this.background.alpha = 0;
+            this.logo = this.add.sprite(this.world.centerX, -300, 'logo');
+            this.logo.anchor.setTo(0.5, 0.5);
+            this.add.tween(this.background).to({ alpha: 1 }, 2000, Phaser.Easing.Bounce.InOut, true);
+            this.add.tween(this.logo).to({ y: 220 }, 2000, Phaser.Easing.Elastic.Out, true, 2000);
+            this.input.onDown.addOnce(this.fadeOut, this);
+        };
+        MainMenu.prototype.fadeOut = function () {
+            this.add.tween(this.background).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+            var tween = this.add.tween(this.logo).to({ y: 800 }, 2000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startGame, this);
+        };
+        MainMenu.prototype.startGame = function () {
+            this.game.state.start('Level1', true, false);
+        };
+        return MainMenu;
+    }(Phaser.State));
+    FitnessGame.MainMenu = MainMenu;
+})(FitnessGame || (FitnessGame = {}));
+var FitnessGame;
+(function (FitnessGame) {
+    var Preloader = (function (_super) {
+        __extends(Preloader, _super);
+        function Preloader() {
+            _super.apply(this, arguments);
+        }
+        Preloader.prototype.preload = function () {
+            //  Set-up our preloader sprite
+            this.preloadBar = this.add.sprite(200, 250, 'preloadBar');
+            this.load.setPreloadSprite(this.preloadBar);
+            //  Load our actual games assets
+            this.load.image('titlepage', 'assets/titlepage.jpg');
+            this.load.image('logo', 'assets/logo.png');
+            this.load.audio('music', 'assets/title.mp3', true);
+            this.load.spritesheet('simon', 'assets/simon.png', 58, 96, 5);
+            this.load.image('level1', 'assets/level1.png');
+        };
+        Preloader.prototype.create = function () {
+            var tween = this.add.tween(this.preloadBar).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startMainMenu, this);
+        };
+        Preloader.prototype.startMainMenu = function () {
+            this.game.state.start('MainMenu', true, false);
+        };
+        return Preloader;
+    }(Phaser.State));
+    FitnessGame.Preloader = Preloader;
+})(FitnessGame || (FitnessGame = {}));
